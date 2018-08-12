3bd88ee811fcfabc1c6f5320ae504d396ce70fd0
Thomas Briot <thomas.briot82@gmail.com>
2018-08-12 18:09:06 -0400

    Return holdingsmarket value

--- app.py
+++ app.py
@@ -15,32 +15,52 @@ def get_portfolio_holdings(port_id):
             with conn.cursor() as cur:
                 print("cursor up")
                 sql = (
-                    "SELECT *, pos_cost/qty as cost_share FROM "
-                    "(SELECT ticker, SUM(quantity) AS qty, SUM(quantity * price) AS pos_cost "
+                    "SELECT ticker, SUM(quantity) AS qty, SUM(quantity * price) AS pos_cost "
                     "FROM investornetwork.trade "
                     "WHERE portfolio_id = {port_id} "
-                    "GROUP BY ticker) as t "
+                    "GROUP BY ticker "
                     "ORDER BY ticker ASC"
                 )
                 cur.execute(sql.format(port_id=port_id))
                 conn.commit()
                 
                 resp_holdings_list = []
+                q_provider = QuoteProvider(60, conn)
                 for row in cur:
-                    resp_holdings_list.append(get_holding_item_dic(row))
+                    resp_holdings_list.append(get_holding_item_dic(row, q_provider))
+
                 return resp_holdings_list
     except Exception:
         return traceback.format_exc()
-    # q_provider = QuoteProvider(60, conn)
 
 
-def get_holding_item_dic(row):
-    json_row = {}
-    json_row['ticker'] = row[0]
-    json_row['quantity'] = row[1]
-    json_row['avg_cost_per_share'] = row[3]
-    json_row['position_cost'] = row[2]
-    return json_row
+def get_holding_item_dic(row, quote_provider):
+    h = {
+        'ticker': row[0],
+        'quantity': float(row[1]),
+        'avg_cost_per_share': fmt_share_price(float(row[2]) / float(row[1])),
+        'position_cost': fmt_price(float(row[2])),
+        'market_value': {},
+        'returns': {}
+    }
+
+    h['market_value']['share_price'] = fmt_share_price(quote_provider.get_quote(h['ticker']))
+    h['market_value']['position'] = fmt_price(h['quantity'] * h['market_value']['share_price'])
+
+    h['returns']['capital_gains'] = fmt_price(h['market_value']['position'] - h['position_cost'])
+    h['returns']['capital_gains_pct'] = fmt_pct(h['returns']['capital_gains'] / h['market_value']['position'])
+
+    return h
+
+def fmt_share_price(price):
+    return float('%.3f' % price)
+
+def fmt_price(price):
+    return float('%.2f' % price)
+
+def fmt_pct(pct):
+    pct = pct * 100
+    return float('%.2f' % pct)
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
