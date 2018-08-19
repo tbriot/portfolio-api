a1ad60d591b8bfccc3d19f69ffe95e53f8a1a32f
Thomas Briot <thomas.briot82@gmail.com>
2018-08-19 08:00:13 -0400

    Added QT get_symbol_by_id method + cached version

--- QuestradeClient.py
+++ QuestradeClient.py
@@ -7,10 +7,14 @@ from CacheClient import CacheClient
 BASE_URL = "https://login.questrade.com"
 TOKEN_URI = BASE_URL + "/oauth2/token"
 ACCOUNT_ACTIVITIES_REL_URI = "v1/accounts/{account_id}/activities?startTime={start_time}&endTime={end_time}&"
+MARKET_SYMBOL_ID_REL_URI = "v1/symbols/{id}"
 
-CACHE_STORE = "qt_token"
-CACHE_ENTRIES_TTL = int(3600*24*90)
+CACHE_STORE_TOKEN = "qt_token"
+CACHE_TTL_TOKEN = int(3600*24*90)
 EXPIRY_DATE_FMT = r"%Y-%m-%d %H:%M:%S"
+
+CACHE_STORE_SYMBOL_BY_ID = "qt_symbol_by_id"
+CACHE_TTL_SYMBOL = 3600*24*7
 
 class QuestradeClient():
     def __init__(self, cache_client, user_id):
@@ -25,6 +29,21 @@ class QuestradeClient():
             end_time = self.qt_time_format(end_date)
         )
         return self.qt_url_get(rel_url)
+
+    def get_symbol_by_id_cached(self, id):
+        r = self._cache_client.get(CACHE_STORE_SYMBOL_BY_ID, id)
+        if r:
+            return json.loads(r)
+        else:
+            s = self.get_symbol_by_id(id)
+            self._cache_client.put(CACHE_STORE_SYMBOL_BY_ID, id, json.dumps(s), CACHE_TTL_SYMBOL)
+            return s
+    
+    def get_symbol_by_id(self, id):
+        rel_url = MARKET_SYMBOL_ID_REL_URI.format(id=id)
+         # returns a dict, the dict first item (key = "symbols") is an array of one item ("symbol")
+        r = self.qt_url_get(rel_url)
+        return r['symbols'][0]
 
     @staticmethod
     def qt_time_format(date):
@@ -61,14 +80,14 @@ class QuestradeClient():
         dt = datetime.now() + timedelta(seconds=new_token_info['expires_in'])
         new_token_info['expiry_date'] = dt.strftime(EXPIRY_DATE_FMT)
         # save token info in remote cache
-        self._cache_client.put(CACHE_STORE, self._user_id, json.dumps(new_token_info), CACHE_ENTRIES_TTL)
+        self._cache_client.put(CACHE_STORE_TOKEN, self._user_id, json.dumps(new_token_info), CACHE_TTL_TOKEN)
         # save token info in memory
         self._token = new_token_info
 
         return self._token['access_token']
 
     def get_token_info_from_cache(self):
-        cached_value = self._cache_client.get(CACHE_STORE, self._user_id)
+        cached_value = self._cache_client.get(CACHE_STORE_TOKEN, self._user_id)
         if cached_value:
             return json.loads(cached_value)
         else:
@@ -87,11 +106,12 @@ class QuestradeClient():
 
 if __name__ == "__main__":
     import os
+    from CacheClient import CacheClient
     os.environ['CACHE_DB_PASSWORD'] = "irondesk89"
     os.environ['CACHE_DB_NAME'] = "investornetwork"
-    from CacheClient import CacheClient
+
     cache_client = CacheClient()
     qt_client = QuestradeClient(cache_client, "1")
-    # print("QT access token=" + qt_client.get_access_token())
-    r = qt_client.get_account_activities("26829536", "2018-08-01", "2018-08-25")
+    #r = qt_client.get_account_activities("26829536", "2018-08-01", "2018-08-25")
+    r = qt_client.get_symbol_by_id_cached(8049)
     print(str(r))
