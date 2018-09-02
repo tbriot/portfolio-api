30860aa65b6f87b821ef471172d393d30fd35f17
Mac Admin <thomas.briot82@gmail.com>
2018-09-02 21:25:59 +0800

    test to see if commit under proper usr on mac

--- chalicelib/test_QuoteProvider.py
+++ chalicelib/test_QuoteProvider.py
@@ -1,12 +1,7 @@
 import unittest
-from QuoteProvider import QuoteProvider
-from sshtunnel import SSHTunnelForwarder
+from chalicelib.QuoteProvider import QuoteProvider
 import pymysql
 from datetime import datetime, timedelta
-
-SSH_HOST = 'ec2-35-183-37-105.ca-central-1.compute.amazonaws.com'
-SSH_USERNAME = 'ec2-user'
-SSH_KEY_FILE = r'C:\Users\Thomas\.ssh\tbriot-ca-central-1.pem'
 
 DB_HOST = 'investornetwork.co0pqf5yoscl.ca-central-1.rds.amazonaws.com'
 DB_USERNAME = 'admin'
@@ -25,38 +20,21 @@ class TestQuoteProvider(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-    def get_ssh_tunnel(self):
-        return SSHTunnelForwarder(
-            (SSH_HOST, 22),
-            ssh_username = SSH_USERNAME,
-            ssh_private_key = SSH_KEY_FILE,
-            remote_bind_address=(DB_HOST, 3306)
-        )
-    
-    def get_db_conn(self, tunnel):
-        return pymysql.connect(
-                LOCALHOST, user=DB_USERNAME,
-                port=tunnel.local_bind_port,
-                passwd=DB_PASSWORD, db=DB_NAME, connect_timeout=5
-        )
-
     def test_get_quote_from_cache(self):
-        with self.get_ssh_tunnel() as tunnel:
-            conn = self.get_db_conn(tunnel)
+            conn = self.get_db_conn()
             q_provider = QuoteProvider(60, conn)
             price, updated_on = q_provider.get_quote_from_cache('AAPL')
             self.assertEqual(price, 207.53)
 
     def test_set_quote_from_cache(self):
-        with self.get_ssh_tunnel() as tunnel:
-            conn = self.get_db_conn(tunnel)
+            conn = self.get_db_conn()
             q_provider = QuoteProvider(60, conn)
 
             q_provider.set_quote_in_cache('BNS', 76.33)
             price, updated_on_1 = q_provider.get_quote_from_cache('BNS')
             self.assertEqual(price, 76.33)
-
             q_provider.set_quote_in_cache('BNS', 76.50)
+
             price, updated_on_2 = q_provider.get_quote_from_cache('BNS')
             self.assertEqual(price, 76.50)
             self.assertNotEqual(updated_on_1, updated_on_2)        
@@ -67,8 +45,7 @@ class TestQuoteProvider(unittest.TestCase):
         self.assertEqual(type(price), float)
 
     def test_get_quote(self):
-        with self.get_ssh_tunnel() as tunnel:
-            conn = self.get_db_conn(tunnel)
+            conn = self.get_db_conn()
             q_provider = QuoteProvider(10, conn)    
 
             price = q_provider.get_quote('GOOG')
